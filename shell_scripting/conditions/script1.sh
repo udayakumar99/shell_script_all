23dd24264bda39f3fdff6910838337f4e0beddb9
EC2 Default User <ec2-user@ip-172-31-4-160.ap-south-1.compute.internal>
2024-01-22 09:06:19 +0000

    adding files

---
+++ shell_scripting/conditions/script1.sh
@@ -0,0 +1,16 @@
+#! bin/bash
+
+echo "enter sal"
+read sal
+if [ $sal -lt 20000 ];then
+	echo "no tax"
+elif [ $sal -gt 20000 -a $sal -lt 40000 ];
+then
+	echo "tax is 5%"
+elif [ $sal -ge 40000 ];
+then
+	echo "tax ix 10"
+elif [ $sal -eq 20000 ];
+then
+	echo "tax is 2%"
+fi
